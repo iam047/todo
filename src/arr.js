d87539ec3f80bd4c58e80d5d51f9236dea411354
Your Name <iam047iam@gmail.com>
2018-03-12 20:16:44 +0200

    audit

---
+++ src/arr.js
@@ -0,0 +1,2 @@
+ const arr = ['Ann','Igor','Andrey','Yaroslav','Aslan','Iam','How i Am'];
+ export default arr;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './main';
+
+
+
+function App () {
+    return (
+        <div>
+            <p>work</p>
+            <Main />
+        </div>
+
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
